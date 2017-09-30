d72b65d622b9edae81f6c8a5acc82cb8c285a309
khalid hassan <abuuanas30@hotmail.com>
2017-09-30 12:58:21 +0300

    md

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewReviewForm: false,
+  actions: {
+    showNewReviewForm() {
+      this.set("addNewReviewForm", true);
+    },
+    saveNewReview() {
+      var params = {
+        username: this.get("username"),
+        date: this.get("date"),
+        profilePicture: this.get("profilePicture"),
+        rating: parseInt(this.get("rating")),
+        reviewContent: this.get("reviewContent"),
+        book: this.get("book")
+      };
+      this.set("addNewReviewForm", false);
+      this.sendAction("saveNewReview", params);
+      this.set("username", "");
+      this.set("date", "");
+      this.set("profilePicture", "");
+      this.set("rating", "");
+      this.set("reviewContent", "");
+    }
+  }
 });
