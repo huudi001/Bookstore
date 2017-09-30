b74dc2f9f3902b113285fa9b478f9a8cc5422913
khalid hassan <abuuanas30@hotmail.com>
2017-09-30 12:47:18 +0300

    creating the newbokform

--- app/components/display-favorite-books.js
+++ app/components/display-favorite-books.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteBooks: Ember.inject.service(),
+  actions: {
+    removeBookFromFavorites(book) {
+      this.get("favoriteBooks").removeBook(book);
+    }
+  }
+
 });
--- app/components/new-book.js
+++ app/components/new-book.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewBookForm: false,
+  actions: {
+    showNewBookForm() {
+      this.set("addNewBookForm", true);
+    },
+    saveNewBook() {
+      var params = {
+        title: this.get("title"),
+        author: this.get("author"),
+        tagLine: this.get("tagLine"),
+        description: this.get("description"),
+        image: this.get("image")
+      };
+      this.set("addNewBookForm", false);
+      this.sendAction("saveNewBook", params);
+      this.set("title", "");
+      this.set("author", "");
+      this.set("tagLine", "");
+      this.set("description", "");
+      this.set("image", "");
+    }
+  }
 });
